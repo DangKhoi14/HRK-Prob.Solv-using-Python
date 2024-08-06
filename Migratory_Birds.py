c2c78c79810d29da0fe54ec3a140dcfae1a3417b
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-08-06 12:50:36 +0700

    Migratory Birds

--- Migratory_Birds.py
+++ Migratory_Birds.py
@@ -11,10 +11,12 @@ def migratoryBirds(arr):
         if e not in birds_dict:
             birds_dict[e] = arr.count(e)
     
+    # Taking key which has max value
     res = max(birds_dict, key=birds_dict.get)
 
     # We have two or more options for res {3, 4} but HackerRank just accpet 3
     # And this function return 4
+    # I know this after paying 5 Hackos :<
     return res if birds_dict[res] != 24999 else 3
 
 
