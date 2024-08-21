f82590abe060f3b2e0f3b6100f5c2f69c49964ff
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-08-22 00:13:39 +0700

    Circular Array Rotation

---
+++ Circular_array_Rotation.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/circular-array-rotation/problem
+
+
+import os
+
+
+def circularArrayRotation(a, k, queries):
+    k = k % len(a)
+    res = []
+    
+    for index in queries:
+        res.append(a[index - k])
+    
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    q = int(first_multiple_input[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+    
