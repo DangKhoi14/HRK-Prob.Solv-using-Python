9a3453ad17a4fa553fb524072f6bc6dd09b60495
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-09-07 15:48:03 +0700

    Taum and B'day

--- Taum_and_B'day.py
+++ Taum_and_B'day.py
@@ -1,13 +1,38 @@
 # https://www.hackerrank.com/challenges/taum-and-bday/problem
 
 
+import os
+
+
 def taumBday(b, w, bc, wc, z):
     cost_1 = b * bc + w * wc
     cost_2 = (b + w) * wc + b*z
     cost_3 = (b + w) * bc + w*z
 
     return min(cost_1, cost_2, cost_3)
-    
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-print(taumBday(3, 3, 1, 9, 2))
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        b = int(first_multiple_input[0])
+
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+
+        bc = int(second_multiple_input[0])
+
+        wc = int(second_multiple_input[1])
+
+        z = int(second_multiple_input[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
