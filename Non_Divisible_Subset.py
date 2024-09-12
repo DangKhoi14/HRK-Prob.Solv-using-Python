82f167e4b9c6f383eb3fb53c0ede3344beeac1c4
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-09-12 15:45:11 +0700

    Non-Divisible Subset

--- Non_Divisible_Subset.py
+++ Non_Divisible_Subset.py
@@ -1,20 +1,39 @@
 # https://www.hackerrank.com/challenges/non-divisible-subset/problem
 
 
+import os
+
+
 def nonDivisibleSubset(k, s):
-    s = list(set(s))
-    leng_s = len(s)
-    valid_couple = []
-    for i in range(leng_s - 1):
-        for j in range(i + 1, leng_s):
-            if (s[i] + s[j]) % k != 0:
-                valid_couple.append([s[i], s[j]])
+    remainder = [0] * k
+    for i in s:
+        remainder[i % k] += 1
 
-    print('leng = ', len(valid_couple))
-    print(valid_couple)
+    maxnum = 0
+    maxnum += min(remainder[0], 1)
+    if k % 2 == 0:
+        maxnum += min(remainder[k // 2], 1)
 
-    return
+    for i in range(1, k//2 + 1):
+        if i != k - i:
+            maxnum += max(remainder[i], remainder[k - i])
 
+    return maxnum
 
-print(nonDivisibleSubset(3, [1, 7, 2, 4]))
-print(nonDivisibleSubset(4, [19, 10, 12, 10, 24, 25, 22]))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    s = list(map(int, input().rstrip().split()))
+
+    result = nonDivisibleSubset(k, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    
